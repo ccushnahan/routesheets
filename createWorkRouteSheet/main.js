779eb4cd3ece1ec546730217faff084238b5bd5d
ccush <synonymsfornotverygood@gmail.com>
2020-12-24 20:57:30 +0000

    new function

--- createWorkRouteSheet/main.js
+++ createWorkRouteSheet/main.js
@@ -1,108 +1,157 @@
 const fs = require("fs");
-const ExcelJS = require("exceljs")
+const ExcelJS = require("exceljs");
 
 function getTotals(entries) {
-    const openings = entries.filter(ent => ent.type == "o");
-    const annuals = entries.filter(ent => ent.type == "a");
-    const quarterlys = entries.filter(ent => ent.type == "q");
-    const monthlys = entries.filter(ent => ent.type == "m");
-    const specials = entries.filter(ent => ent.type == "s");
+  const openings = entries.filter((ent) => ent.type == "o");
+  const annuals = entries.filter((ent) => ent.type == "a");
+  const quarterlys = entries.filter((ent) => ent.type == "q");
+  const monthlys = entries.filter((ent) => ent.type == "m");
+  const specials = entries.filter((ent) => ent.type == "s");
 
-    const totals = {
-        quarterlyReads: sumEntries(quarterlys),
-        monthlyReads: sumEntries(monthlys),
-        annualReads: sumEntries(annuals),
-        specialReads: sumEntries(specials), 
-        openingReads: sumEntries(openings),
-    };
+  const totals = {
+    quarterlyReads: sumEntries(quarterlys),
+    monthlyReads: sumEntries(monthlys),
+    annualReads: sumEntries(annuals),
+    specialReads: sumEntries(specials),
+    openingReads: sumEntries(openings),
+  };
 
-    return totals
+  return totals;
 }
 
 function sumEntries(entries) {
-    let attempts = entries.reduce((total, entry) => total + entry.attempts, 0)
-    let complete = entries.reduce((total, entry) => total + entry.complete, 0)
+  let attempts = entries.reduce((total, entry) => total + entry.attempts, 0);
+  let complete = entries.reduce((total, entry) => total + entry.complete, 0);
 
-    return [attempts, complete, attempts - complete];
+  return [attempts, complete, attempts - complete];
 }
 
-
 function readCSV(fileName) {
-    return fs.readFileSync("/home/cush/projects/workRouteSheet/reads.csv", "utf8").split("\n")
+  return fs
+    .readFileSync("/home/cush/projects/workRouteSheet/reads.csv", "utf8")
+    .split("\n");
 }
 
 function getEntriesWithinWeek(entries, date = new Date()) {
-    const WEEKLENGTH = 604800*1000
-    const endWeek = date.getTime();
-    const startWeek = new Date(endWeek - WEEKLENGTH).getTime();
-    const weekReads = entries.filter(entry => entry.timeStamp <= endWeek && entry.timeStamp >= startWeek)
-    return weekReads;
+  const WEEKLENGTH = 604800 * 1000;
+  const endWeek = date.getTime();
+  const startWeek = new Date(endWeek - WEEKLENGTH).getTime();
+  const weekReads = entries.filter(
+    (entry) => entry.timeStamp <= endWeek && entry.timeStamp >= startWeek
+  );
+  return weekReads;
 }
-
 
 class readEntry {
-    constructor(entryString) {
-        let entryArr = entryString.split(",")
-        this.timeStamp = entryArr[0];
-        this.type = entryArr[1];
-        this.name = entryArr[2];
-        this.attempts = +entryArr[3];
-        this.complete = +entryArr[4];
-        this.setFails();
-        this.postcodes = entryArr.slice(5);
-    }
+  constructor(entryString) {
+    let entryArr = entryString.split(",");
+    this.timeStamp = entryArr[0];
+    this.type = entryArr[1];
+    this.name = entryArr[2];
+    this.attempts = +entryArr[3];
+    this.complete = +entryArr[4];
+    this.setFails();
+    this.postcodes = entryArr.slice(5);
+    this.date = new Date(this.timeStamp);
+  }
 
-    setFails() {
-        this.fails =  this.attempts - this.complete;
-    }
+  getDate() {
+    let date = this.date;
+    return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+  }
 
-    getFails() {
-        console.log(this.attempts, this.complete)
-        return this.attempts - this.complete
-    }
+  getDay() {
+    const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+    return days[this.date.getDay()];
+  }
+
+  setFails() {
+    this.fails = this.attempts - this.complete;
+  }
+
+  getFails() {
+    console.log(this.attempts, this.complete);
+    return this.attempts - this.complete;
+  }
 }
 
-
 function addTotalsToSheet(weeksTotals, sheet) {
-    const totalStartCol = 6
-    let totalRowNum = 7;
+  const totalStartCol = 6;
+  let totalRowNum = 7;
 
-    Object.values(weeksTotals).forEach(total => {
-        let currentCol = totalStartCol;
-        const row = sheet.getRow(totalRowNum)
-        total.forEach(value => {
-            row.getCell(currentCol).value = value;
-            currentCol++;
-        })
-        totalRowNum++;
-    })
+  Object.values(weeksTotals).forEach((total) => {
+    let currentCol = totalStartCol;
+    const row = sheet.getRow(totalRowNum);
+    total.forEach((value) => {
+      row.getCell(currentCol).value = value;
+      currentCol++;
+    });
+    totalRowNum++;
+  });
+}
+
+function addReadsToSheet(weekEntries, sheet) {
+  let currentDate;
+  let currentDay;
+  weekEntries.forEach((entry, i) => {
+    let currentRow = 4 + i;
+    let rowVals = [
+      "",
+      "",
+      entry.postcodes,
+      "",
+      entry.name,
+      entry.attempts,
+      entry.complete,
+      entry.fails,
+    ];
+    if (entry.getDay != currentDay) {
+      currentDate = entry.getDate();
+      currentDay = entry.getDay();
+      rowVals[0] = currentDay;
+      rowVals[1] = currentDate;
+    }
+
+    sheet.splice(currentRow, 0, rowVals);
+  });
 }
 
 async function createRouteSheet() {
-    // open and load template
-    const templateFile = "/home/cush/projects/workRouteSheet/createWorkRouteSheet/template.xlsx";
-    const templateBook = new ExcelJS.Workbook();
-    await templateBook.xlsx.readFile(templateFile);
-    
-    // get worksheet
-    const sheet = templateBook.worksheets[0];
+  // open and load template
+  const templateFile =
+    "/home/cush/projects/workRouteSheet/createWorkRouteSheet/template.xlsx";
+  const templateBook = new ExcelJS.Workbook();
+  await templateBook.xlsx.readFile(templateFile);
 
-    // process csv file for info
-    let csvEntries = readCSV();
-    const entries = csvEntries.filter(line => line != "").map(line => new readEntry(line));
-    const weekEntries = getEntriesWithinWeek(entries);
-    const weeksTotals = getTotals(weekEntries);
+  // get worksheet
+  const sheet = templateBook.worksheets[0];
 
-    // Add totals then entries to file (totals first because rows won't be consistant week to week)
-    addTotalsToSheet(weeksTotals, sheet)
+  // process csv file for info
+  let csvEntries = readCSV();
+  const entries = csvEntries
+    .filter((line) => line != "")
+    .map((line) => new readEntry(line));
+  const weekEntries = getEntriesWithinWeek(entries);
+  const weeksTotals = getTotals(weekEntries);
 
-    // get fileName
-    const date = new Date();
-    const fileName = `Routesheet-${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.xlsx` 
+  // Add totals then entries to file (totals first because rows won't be consistant week to week)
+  addTotalsToSheet(weeksTotals, sheet);
+  addReadsToSheet(weekEntries, sheet);
+  // get fileName
+  const date = new Date();
+  const fileName = `Routesheet-${date.getDate()}-${date.getMonth()}-${date.getFullYear()}.xlsx`;
 
-    // save new file
-    const newBook = templateBook;
-    await newBook.xlsx.writeFile(fileName);
+  // save new file
+  const newBook = templateBook;
+  await newBook.xlsx.writeFile(fileName);
 }
 
 createRouteSheet();
