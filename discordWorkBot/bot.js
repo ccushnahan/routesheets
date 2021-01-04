c5aebaf92b5f41407b7534cd705331234d11eab4
Synonymsfornotverygood <Synonymsfornotverygood@gmail.com>
2021-01-04 21:41:13 +0000

    Fixed message infinite loop

--- discordWorkBot/bot.js
+++ discordWorkBot/bot.js
@@ -14,36 +14,41 @@ client.once("ready", () => console.log("ready"));
 client.login(process.env.TOKEN)
 
 client.on("message", message => {
-    let messageContent = message.content.toUpperCase()
-    if (!recordMessages) {
-        if (messageContent == "!START") {
-            message.reply("Enter reads (readtype..readname..attempts..complete..postcodes)")
-            console.log("started");
-            recordMessages = true;
-        } 
-    } else if (recordMessages) {
-        if (message.content == "!END") {
-            message.reply("Finished entering reads.")
-            console.log("ended")
-            recordMessages = false;
-        } else {
-            const date = new Date();
-            const sectCount = messageContent.match(/[.][.]/gi) || 0;
-            if (sectCount < 4) {
-                message.reply(`You submitted: ${messageContent}`)
-                message.reply(`This doesn't seem to be in the correct format`)
-                message.reply(`Please enter this again (readtype..readname..attempts..complete..postcodes)`)
+    if (message.author.username != "workbot") {
+        let messageContent = message.content.toUpperCase();
+        let reply = "";
+        if (!recordMessages) {
+            if (messageContent == "!START") {
+                reply = "Enter reads (readtype..readname..attempts..complete..postcodes)"
+                console.log("started");
+                recordMessages = true;
+            } 
+        } else if (recordMessages) {
+            if (messageContent == "!END") {
+                reply = "Finished entering reads."
+                console.log("ended")
+                recordMessages = false;
             } else {
-                const csvEntry = date.getTime() + "," + messageContent.split("..").join(",") + "\n";
-                console.log(csvEntry)
-                message.reply(`Read entered: ${messageContent}`)
-                fs.appendFile(`${path.join(home, "/readsRecords/reads.csv")}`, csvEntry, (err) => {
-                    console.log(err);
-                    message.reply(`Something went wrong: ${err}`)
-                    message.reply("Try again")
-                })
+                const date = new Date();
+                const sectCount = messageContent.match(/[.][.]/gi) || 0;
+                if (sectCount < 4) {
+                    reply = `You submitted: ${messageContent}\n`
+                    reply += `This doesn't seem to be in the correct format\n`
+                    reply += `Please enter this again (readtype..readname..attempts..complete..postcodes)`
+                } else {
+                    const csvEntry = date.getTime() + "," + messageContent.split("..").join(",") + "\n";
+                    console.log(csvEntry)
+                    reply = `Read entered: ${csvEntry}`;
+                    fs.appendFile(`${path.join(home, "/readsRecords/reads.csv")}`, csvEntry, (err) => {
+                        console.log(err);
+                        reply += `\nSomething went wrong: ${err}\nTry again`
+                    })
+                }
             }
         }
+        if (reply.length > 0) {
+            message.reply(reply)
+        } 
     }
     console.log(message.content);
 })
